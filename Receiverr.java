dc4578ae6e6fc404b3b39b285c1a3914f6d20a3d
vybhaviii <55616072+vybhaviii@users.noreply.github.com>
2019-10-12 16:17:52 +0530

    Receiver code with proper ack sent

---
+++ Receiverr.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.net.*;
+
+public class Receiverr{
+ServerSocket reciever;
+Socket connection=null;
+ObjectOutputStream out;
+ObjectInputStream in;
+String packet,ack,data="";
+int i=0,sequence=0;
+
+Receiverr(){}
+
+public void run(){
+try{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+reciever = new ServerSocket(20084,10);
+System.out.println("waiting for connection...");
+connection=reciever.accept();
+sequence=0;
+System.out.println("Connection established   :");
+out=new ObjectOutputStream(connection.getOutputStream());
+out.flush();
+in=new ObjectInputStream(connection.getInputStream());
+out.writeObject("connected.");
+do{
+try{
+packet=(String)in.readObject();
+if(Integer.valueOf(packet.substring(0,1))==sequence){
+data+=packet.substring(1);
+sequence=(sequence==0)?1:0;
+System.out.println("\n\nreceiver         >"+packet);
+}
+else
+{
+System.out.println("\n\nreceiver         >"+packet +"   duplicate data");
+}
+//if(i<3){  //all this commented part helps to send ack with wrong seq no.
+Thread.sleep(2000);
+out.writeObject(String.valueOf(sequence));
+//i++;
+//}
+//else{
+//out.writeObject(String.valueOf((sequence+1)%2));
+//i=0;
+//}
+}
+catch(Exception e){}
+}
+while(!packet.equals("end"));
+System.out.println("Data received="+data);
+out.writeObject("connection ended    .");
+}
+catch(Exception e){}
+finally{
+try{
+in.close();
+out.close();
+reciever.close();
+}
+catch(Exception e){}
+}
+}
+
+public static void main(String args[]){
+Receiverr s=new Receiverr();
+
+s.run();
+
+}
+}
